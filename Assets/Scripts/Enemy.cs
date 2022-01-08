1b96740b6e5b96433c01186bd53e97b19268e715
Dylan Hicks <dman1366@gmail.com>
2022-01-08 16:48:26 -0700

    Create enemy and enemy hp bar.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Configs
+    [SerializeField] int maxHealth = 100;
+    [SerializeField] HealthBar healthBarUI = null;
+
+    // State
+    int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBarUI.Initialize(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // TODO: remove this test code
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(20);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            TakeHealing(20);
+        }
+
+        healthBarUI.SetHealth(currentHealth);
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+    private void TakeHealing(int healing)
+    {
+        currentHealth += healing;
+
+        if (maxHealth < currentHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
